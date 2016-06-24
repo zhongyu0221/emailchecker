30a3e2e65a03e2c0a95c01b8aeb1537cd5043a7d
zhongyu0221 <zhongyu0221@gmail.com>
2016-06-24 09:16:15 +0800

    single thread done

--- Documents/cs 3330/EmailChecker/src/emailchecker/FXMLDocumentController.java
+++ Documents/cs 3330/EmailChecker/src/emailchecker/FXMLDocumentController.java
@@ -19,6 +19,11 @@ import javafx.scene.text.Text;
         
 
 
+import org.apache.commons.net.smtp.SMTPClient;
+import org.apache.commons.net.smtp.SMTPReply;
+import org.xbill.DNS.Lookup;
+import org.xbill.DNS.Type;
+
 /**
  *
  * @author zhongyu
@@ -31,66 +36,35 @@ public class FXMLDocumentController implements Initializable {
     @FXML
     private Button checkbtn;
     @FXML
+    private Text addressarea;
+    @FXML
     private Text resultarea;
+    
+    
+     String EmailAdd;
+    Boolean result;
+    
+   CheckEmailObj EmailObj = new CheckEmailObj();
     
     @FXML
     private void handleButtonAction(ActionEvent event) {
        /* System.out.println("You clicked me!");
         resultarea.setText("Hello World!");*/
+       EmailAdd= inputemail.getText();
+      addressarea.setText(EmailAdd);
+     result = EmailObj.checkEmail(EmailAdd);
+       resultarea.setText(result.toString());
        
        
     }
     
-    public static boolean checkEmail(String email) {
-        if (!email.matches("[\\w\\.\\-]+@([\\w\\-]+\\.)+[\\w\\-]+")) {
-            return false;
-        }
- 
-        String host = "";
-        String hostName = email.split("@")[1];
-        Record[] result = null;
-        SMTPClient client = new SMTPClient();
- 
-        try {
-            // 查找MX记录
-            Lookup lookup = new Lookup(hostName, Type.MX);
-            lookup.run();
-            if (lookup.getResult() != Lookup.SUCCESSFUL) {
-                return false;
-            } else {
-                result = lookup.getAnswers();
-            }
- 
-            // 连接到邮箱服务器
-            for (int i = 0; i < result.length; i++) {
-                host = result[i].getAdditionalName().toString();
-                client.connect(host);
-                if (!SMTPReply.isPositiveCompletion(client.getReplyCode())) {
-                    client.disconnect();
-                    continue;
-                } else {
-                    break;
-                }
-            }
- 
-            //以下2项自己填写快速的，有效的邮箱
-            client.login("163.com");
-            client.setSender("sxgkwei@163.com");
-            client.addRecipient(email);
-            if (250 == client.getReplyCode()) {
-                return true;
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                client.disconnect();
-            } catch (IOException e) {
-            }
-        }
-        return false;
-    }
-    
+   
+
+
+
+
+
+
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
